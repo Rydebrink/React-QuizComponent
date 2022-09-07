41e2a68b5c6bac3877a970620bc5c23778d02b2c
Rydebrink <simon@rydebrink.se>
2022-09-07 09:04:07 +0200

    Module6

--- src/QuizQuestion.js
+++ src/QuizQuestion.js
@@ -2,9 +2,23 @@ import React, { Component } from "react";
 import QuizQuestionButton from "./QuizQuestionButton.js";
 
 class QuizQuestion extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      incorrectAnswer: false,
+    };
+  }
   handleClick(buttonText) {
     if (buttonText === this.props.quiz_question.answer) {
+      this.setState((state) => {
+        return { incorrectAnswer: false };
+      });
       this.props.showNextQuestionHandler();
+    } else {
+      this.setState((state) => {
+        return { incorrectAnswer: true };
+      });
     }
   }
   render() {
@@ -28,6 +42,9 @@ class QuizQuestion extends Component {
             )}
           </ul>
         </section>
+        {this.state.incorrectAnswer ? (
+          <p className="error">Sorry, that's not right</p>
+        ) : null}
       </main>
     );
   }
